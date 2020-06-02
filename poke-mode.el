af4cc87816db2666e61ba02196d938a4acbebfce
Ryan Miller <ryan@devopsmachine.com>
2020-06-02 01:53:22 -0400

    Update documentation

--- poke-mode.el
+++ poke-mode.el
@@ -24,28 +24,14 @@
 
 ;;; Commentary:
 
-;; NEW! You can now click on the rainbow (or the empty space)
-;; to scroll your buffer!
+;; You can now click on the rainbow (or the empty space) to scroll
+;; your buffer!
 
-;; NEW! You can now customize the minimum window width
-;; below which the nyan-mode will be disabled, so that more important
-;; information can be shown in the modeline.
+;; You can now customize the minimum window width below which the
+;; nyan-mode will be disabled, so that more important information
+;; can be shown in the modeline.
 
 ;; To activate, just load and put `(nyan-mode 1)' in your init file.
-
-;; Contributions and feature requests welcome!
-
-;; Inspired by (and in few places copied from) sml-modeline.el written by Lennart Borgman.
-;; See: http://bazaar.launchpad.net/~nxhtml/nxhtml/main/annotate/head%3A/util/sml-modeline.el
-
-;;; History:
-
-;; 2016-04-26 - introduced click-to-scroll feature.
-
-;; Started as a totally random idea back in August 2011.
-
-;; The homepage at http://nyan-mode.buildsomethingamazing.com died somewhen in 2014/2015 because reasons.
-;; I might get the domain back one day.
 
 ;;; Code:
 
