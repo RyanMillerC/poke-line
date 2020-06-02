649778d38742c76ea02db83caab4fed1520f4df9
Ryan Miller <ryan@devopsmachine.com>
2020-06-02 15:48:15 -0400

    Replace Nyan references with poke

--- poke-mode.el
+++ poke-mode.el
@@ -24,259 +24,149 @@
 
 ;;; Commentary:
 
-;; You can now click on the rainbow (or the empty space) to scroll
+;; To activate, load and put `(poke-mode 1)' in your init file.
+
+;; You can click on the position bar (or the empty space) to scroll
 ;; your buffer!
 
-;; You can now customize the minimum window width below which the
-;; nyan-mode will be disabled, so that more important information
-;; can be shown in the modeline.
-
-;; To activate, just load and put `(nyan-mode 1)' in your init file.
+;; You can customize the minimum window width below which poke-mode
+;; will be disabled, so that more important information can be shown
+;; in the modeline.
 
 ;;; Code:
 
 (eval-when-compile (require 'cl))
 
-(defconst +nyan-directory+ (file-name-directory (or load-file-name buffer-file-name)))
+(defconst +poke-directory+ (file-name-directory (or load-file-name buffer-file-name)))
 
-(defconst +nyan-cat-size+ 3)
+(defconst +poke-size+ 3)
 
-(defconst +nyan-cat-image+ (concat +nyan-directory+ "img/charizard.xpm"))
-(defconst +nyan-rainbow-image+ (concat +nyan-directory+ "img/flamethrower.xpm"))
-(defconst +nyan-outerspace-image+ (concat +nyan-directory+ "img/outerspace.xpm"))
+(defconst +poke-image+ (concat +poke-directory+ "img/charizard.xpm"))
+(defconst +poke-element-image+ (concat +poke-directory+ "img/flamethrower.xpm"))
+(defconst +poke-background-image+ (concat +poke-directory+ "img/background.xpm"))
 
-(defconst +nyan-music+ (concat +nyan-directory+ "mus/nyanlooped.mp3"))
+(defconst +poke-modeline-help-string+ "Gotta catch 'em all!\nmouse-1: Scroll buffer position")
 
-(defconst +nyan-modeline-help-string+ "Nyanyanya!\nmouse-1: Scroll buffer position")
+(defvar poke-old-car-mode-line-position nil)
 
-(defvar nyan-old-car-mode-line-position nil)
-
-(defgroup nyan nil
-  "Customization group for `nyan-mode'."
+(defgroup poke nil
+  "Customization group for `poke-mode'."
   :group 'frames)
 
-(defun nyan-refresh ()
-  "Refresh nyan mode.
+(defun poke-refresh ()
+  "Refresh poke-mode.
 Intended to be called when customizations were changed, to reapply them immediately."
-  (when (featurep 'nyan-mode)
-    (when (and (boundp 'nyan-mode)
-               nyan-mode)
-      (nyan-mode -1)
-      (nyan-mode 1))))
+  (when (featurep 'poke-mode)
+    (when (and (boundp 'poke-mode) poke-mode)
+      (poke-mode -1)
+      (poke-mode 1))))
 
-(defcustom nyan-animation-frame-interval 0.2
-  "Number of seconds between animation frames."
-  :type 'float
-  :set (lambda (sym val)
-         (set-default sym val)
-         (nyan-refresh))
-  :group 'nyan)
-
-(defvar nyan-animation-timer nil)
-
-(defun nyan--is-animating-p ()
-  "T if animating, NIL otherwise."
-  (timerp nyan-animation-timer))
-
-(defun nyan-start-animation ()
-  (interactive)
-  (unless (nyan--is-animating-p)
-    (setq nyan-animation-timer (run-at-time "1 sec"
-                                            nyan-animation-frame-interval
-                                            'nyan-swich-anim-frame))))
-
-(defun nyan-stop-animation ()
-  (interactive)
-  (when (nyan--is-animating-p)
-    (cancel-timer nyan-animation-timer)
-    (setq nyan-animation-timer nil)))
-
-(defcustom nyan-minimum-window-width 64
-  "Minimum width of the window, below which nyan-mode will not be displayed.
-This is important because nyan-mode will push out all informations from small windows."
+(defcustom poke-minimum-window-width 64
+  "Minimum width of the window, below which poke-mode will not be displayed.
+This is important because poke-mode will push out all informations from small windows."
   :type 'integer
   :set (lambda (sym val)
          (set-default sym val)
-         (nyan-refresh))
-  :group 'nyan)
+         (poke-refresh))
+  :group 'poke)
 
-;;; FIXME bug, doesn't work for antoszka.
-(defcustom nyan-wavy-trail nil
-  "If enabled, Nyan Cat's rainbow trail will be wavy."
-  :type '(choice (const :tag "Enabled" t)
-                 (const :tag "Disabled" nil))
-  :set (lambda (sym val)
-         (set-default sym val)
-         (nyan-refresh))
-  :group 'nyan)
-
-(defcustom nyan-bar-length 32
-  "Length of Nyan Cat bar in units.
+(defcustom poke-bar-length 32
+  "Length of Poke element bar in units.
 Each unit is equal to an 8px image.
-Minimum of 3 units are required for Nyan Cat."
+Minimum of 3 units are required for poke-mode."
   :type 'integer
   :set (lambda (sym val)
          (set-default sym val)
-         (nyan-refresh))
-  :group 'nyan)
+         (poke-refresh))
+  :group 'poke)
 
-(defcustom nyan-animate-nyancat nil
-  "Enable animation for Nyan Cat.
-This can be t or nil."
-  :type '(choice (const :tag "Enabled" t)
-                 (const :tag "Disabled" nil))
-  ;; FIXME: Starting an animation timer on defcustom isn't a good idea; this needs to, at best, maybe start/stop a timer iff the mode is on,
-  ;; otherwise just set a flag. -- Jacek Złydach, 2020-05-26
-  :set (lambda (sym val)
-         (set-default sym val)
-         (if val
-             (nyan-start-animation)
-           (nyan-stop-animation))
-         (nyan-refresh))
-  :group 'nyan)
-
-(defcustom nyan-cat-face-number 1
+(defcustom poke-selector 1
   "Select cat face number for console."
   :type 'integer
-  :group 'nyan
-  )
+  :group 'poke)
 
-;;; Load images of Nyan Cat an it's rainbow.
-(defvar nyan-cat-image (if (image-type-available-p 'xpm)
-                           (create-image +nyan-cat-image+ 'xpm nil :ascent 'center)))
-
-(defvar nyan-animation-frames (if (image-type-available-p 'xpm)
-                                  (mapcar (lambda (id)
-                                            (create-image (concat +nyan-directory+ (format "img/nyan-frame-%d.xpm" id))
-                                                          'xpm nil :ascent 95))
-                                          '(1 2 3 4 5 6))))
-(defvar nyan-current-frame 0)
-
-(defconst +nyan-catface+ [
-                          ["[]*" "[]#"]
-                          ["(*^ｰﾟ)" "( ^ｰ^)" "(^ｰ^ )" "(ﾟｰ^*)"]
-                          ["(´ω｀三 )" "( ´ω三｀ )" "( ´三ω｀ )" "( 三´ω｀)"
-                           "( 三´ω｀)" "( ´三ω｀ )" "( ´ω三｀ )" "(´ω｀三 )"]
-                          ["(´д｀;)" "( ´д`;)" "( ;´д`)" "(;´д` )"]
-                          ["(」・ω・)」" "(／・ω・)／" "(」・ω・)」" "(／・ω・)／"
-                           "(」・ω・)」" "(／・ω・)／" "(」・ω・)」" "＼(・ω・)／"]
-                          ["(＞ワ＜三　　　)" "(　＞ワ三＜　　)"
-                           "(　　＞三ワ＜　)" "(　　　三＞ワ＜)"
-                           "(　　＞三ワ＜　)" "(　＞ワ三＜　　)"]])
-
-(defun nyan-toggle-wavy-trail ()
-  "Toggle the trail to look more like the original Nyan Cat animation."
-  (interactive)
-  (setq nyan-wavy-trail (not nyan-wavy-trail)))
-
-(defun nyan-swich-anim-frame ()
-  (setq nyan-current-frame (% (+ 1 nyan-current-frame) 6))
-  (redraw-modeline))
-
-(defun nyan-get-anim-frame ()
-  (if (nyan--is-animating-p)
-      (nth nyan-current-frame nyan-animation-frames)
-    nyan-cat-image))
-
-(defun nyan-wavy-rainbow-ascent (number)
-  (if (nyan--is-animating-p)
-      (min 100 (+ 90
-                  (* 3 (abs (- (/ 6 2)
-                               (% (+ number nyan-current-frame)
-                                  6))))))
-    (if (zerop (% number 2)) 80 'center)))
-
-(defun nyan-number-of-rainbows ()
+(defun poke-number-of-elements ()
   (round (/ (* (round (* 100
-                         (/ (- (float (point))
-                               (float (point-min)))
-                            (float (point-max)))))
-               (- nyan-bar-length +nyan-cat-size+))
-            100)))
+    (/ (- (float (point))
+      (float (point-min)))
+        (float (point-max)))))
+            (- poke-bar-length +poke-size+))
+          100)))
 
-(defun nyan-catface ()
-  (aref +nyan-catface+ nyan-cat-face-number))
-
-(defun nyan-catface-index ()
+(defun poke-pokemon-index ()
   (min (round (/ (* (round (* 100
-                              (/ (- (float (point))
-                                    (float (point-min)))
-                                 (float (point-max)))))
-                    (length (nyan-catface)))
-                 100)) (- (length (nyan-catface)) 1)))
+    (/ (- (float (point))
+      (float (point-min)))
+        (float (point-max)))))
+          (length (poke-catface)))
+            100))
+    (- (length (poke-catface)) 1)))
 
-(defun nyan-scroll-buffer (percentage buffer)
+(defun poke-scroll-buffer (percentage buffer)
   "Move point `BUFFER' to `PERCENTAGE' percent in the buffer."
   (interactive)
   (with-current-buffer buffer
     (goto-char (floor (* percentage (point-max))))))
 
-(defun nyan-add-scroll-handler (string percentage buffer)
+(defun poke-add-scroll-handler (string percentage buffer)
   "Propertize `STRING' to scroll `BUFFER' to `PERCENTAGE' on click."
   (lexical-let ((percentage percentage)
-                (buffer buffer))
-    (propertize string 'keymap `(keymap (mode-line keymap (down-mouse-1 . ,(lambda () (interactive) (nyan-scroll-buffer percentage buffer))))))))
+                 (buffer buffer))
+    (propertize string 'keymap `(keymap (mode-line keymap (down-mouse-1 . ,(lambda () (interactive) (poke-scroll-buffer percentage buffer))))))))
 
-(defun nyan-create ()
-  "Return the Nyan Cat indicator to be inserted into mode line."
-  (if (< (window-width) nyan-minimum-window-width)
-      ""                                ; disabled for too small windows
-    (let* ((rainbows (nyan-number-of-rainbows))
-           (outerspaces (- nyan-bar-length rainbows +nyan-cat-size+))
-           (rainbow-string "")
-           (xpm-support (image-type-available-p 'xpm))
-           (nyancat-string (propertize
-                            (aref (nyan-catface) (nyan-catface-index))
-                            'display (nyan-get-anim-frame)))
-           (outerspace-string "")
-           (buffer (current-buffer)))
-      (dotimes (number rainbows)
-        (setq rainbow-string (concat rainbow-string
-                                     (nyan-add-scroll-handler
-                                      (if xpm-support
-                                          (propertize "|"
-                                                      'display (create-image +nyan-rainbow-image+ 'xpm nil :ascent (or (and nyan-wavy-trail
-                                                                                                                            (nyan-wavy-rainbow-ascent number))
-                                                                                                                       (if (nyan--is-animating-p) 95 'center))))
-                                        "|")
-                                      (/ (float number) nyan-bar-length) buffer))))
-      (dotimes (number outerspaces)
-        (setq outerspace-string (concat outerspace-string
-                                        (nyan-add-scroll-handler
-                                         (if xpm-support
-                                             (propertize "-"
-                                                         'display (create-image +nyan-outerspace-image+ 'xpm nil :ascent (if (nyan--is-animating-p) 95 'center)))
-                                           "-")
-                                         (/ (float (+ rainbows +nyan-cat-size+ number)) nyan-bar-length) buffer))))
-      ;; Compute Nyan Cat string.
-      (propertize (concat rainbow-string
-                          nyancat-string
-                          outerspace-string)
-                  'help-echo +nyan-modeline-help-string+))))
+(defun poke-create ()
+  "Return the Poke Cat indicator to be inserted into mode line."
+  (if (< (window-width) poke-minimum-window-width)
+    (let* ((elements (poke-number-of-elements))
+      (backgrounds (- poke-bar-length elements +poke-size+))
+      (element-string "")
+      (xpm-support (image-type-available-p 'xpm))
+        (pokemon-string (propertize (aref (poke-face) (poke-face-index)
+          'display (poke-get-anim-frame)))
+            (background-string "")
+            (buffer (current-buffer)))
+        (dotimes (number elements)
+          (setq element-string
+            (concat element-string
+              (poke-add-scroll-handler
+                (if xpm-support
+                    (propertize "|" 'display (create-image +poke-element-image+ 'xpm nil
+                                             :ascent (or (and poke-wavy-trail (poke-wavy-rainbow-ascent number))
+                                                       (if (poke--is-animating-p) 95 'center))))
+                  "|")
+                (/ (float number) poke-bar-length) buffer))))
+        (dotimes (number backgrounds)
+          (setq background-string
+            (concat background-string (poke-add-scroll-handler)
+              (if xpm-support
+                  (propertize "-" 'display (create-image +poke-background-image+ 'xpm nil
+                    :ascent (if (poke--is-animating-p) 95 'center)))
+                "-")
+              (/ (float (+ elements +nyan-cat-size+ number)) nyan-bar-length) buffer)))
+            ;; Compute Poke Cat string.
+            (propertize (concat pokemon-string
+                          element-string
+                          background-string)
+                  'help-echo +poke-modeline-help-string+)))))
 
 ;;;###autoload
-(define-minor-mode nyan-mode
-  "Use NyanCat to show buffer size and position in mode-line.
-You can customize this minor mode, see option `nyan-mode'.
+(define-minor-mode poke-mode
+  "Use Pokemon to show buffer size and position in mode-line.
+You can customize this minor mode, see option `poke-mode'.
 
 Note: If you turn this mode on then you probably want to turn off
 option `scroll-bar-mode'."
   :global t
-  :group 'nyan
-  ;; FIXME: That doesn't smell right; might still get duplicate nyan cats and other mode-line disruptions.  -- Jacek Złydach, 2020-05-26
-  (cond (nyan-mode
-         (unless nyan-old-car-mode-line-position
-           (setq nyan-old-car-mode-line-position (car mode-line-position)))
-         (setcar mode-line-position '(:eval (list (nyan-create))))
-         ;; NOTE Redundant, but intended to, in the future, prevent the custom variable from starting the animation timer even if nyan mode isn't active. -- Jacek Złydach, 2020-05-26
-         (when nyan-animate-nyancat
-           (nyan-start-animation)))
-        ((not nyan-mode)
-         (nyan-stop-animation)          ; In case there was an animation going on.
-         (setcar mode-line-position nyan-old-car-mode-line-position)
-         (setq nyan-old-car-mode-line-position nil))))
+  :group 'poke
+  (cond (poke-mode
+         (unless poke-old-car-mode-line-position
+           (setq poke-old-car-mode-line-position (car mode-line-position)))
+         (setcar mode-line-position '(:eval (list (poke-create))))
+        ((not poke-mode)
+         (setcar mode-line-position poke-old-car-mode-line-position)
+         (setq poke-old-car-mode-line-position nil))))
 
 
-(provide 'nyan-mode)
+(provide 'poke-mode)
 
-;;; nyan-mode.el ends here
+;;; poke-mode.el ends here
