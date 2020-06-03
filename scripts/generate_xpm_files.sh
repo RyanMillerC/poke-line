3c5acc737ed8fd6c1c530f4bc02faabee11fd9cf
Ryan Miller <ryan@devopsmachine.com>
2020-06-03 01:49:19 -0400

    Refactor generation script for types

--- scripts/generate_xpm_files.sh
+++ scripts/generate_xpm_files.sh
@@ -13,15 +13,24 @@ OUTPUT_DIRECTORY='./assets/output'
 
 [[ ! -d "${OUTPUT_DIRECTORY}" ]] && mkdir "${OUTPUT_DIRECTORY}"
 
-for image in $(find "${SPRITES_DIRECTORY}" -name '*.png') ; do
-    image_base_name="${image##*/}"
-    temp_file="/tmp/${image_base_name}"
-    output_file="${OUTPUT_DIRECTORY}/${image_base_name}"
-    echo "Processing ${image} -> ${output_file}"
-    convert "${image}" -flop -trim -resize 20x30 "${temp_file}"
-    convert "${BASE_DIRECTORY}/flamethrower_base.png" -gravity center "${temp_file}" -composite "${output_file}"
-    rm "${temp_file}"
-    open "${output_file}"
+for type_directory in $(find "${SPRITES_DIRECTORY}" ! -path "${SPRITES_DIRECTORY}" -type d) ; do
+    pokemon_type="${type_directory##*/}"
+    for image in $(find "${type_directory}" -name '*.png') ; do
+        image_base_name="${image##*/}"
+        temp_file="/tmp/${image_base_name}"
+        output_file="${OUTPUT_DIRECTORY}/${image_base_name}"
+        echo "Processing ${image} -> ${output_file}"
+        convert "${image}" -flop \
+                           -trim \
+                           -resize '20x30>' \
+                           -unsharp 0x.5 \
+                           "${temp_file}"
+        convert "${BASE_DIRECTORY}/${pokemon_type}_base.png" \
+                -gravity center \
+                "${temp_file}" \
+                -composite "${output_file}"
+        rm "${temp_file}"
+    done
 done
 
 echo "Complete!"
