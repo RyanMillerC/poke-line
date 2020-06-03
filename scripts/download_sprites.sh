28119abc6ecb8e8517ac0e6efe882136a70b8625
Ryan Miller <ryan@devopsmachine.com>
2020-06-03 02:05:39 -0400

    Move API base URL into variable

--- scripts/download_sprites.sh
+++ scripts/download_sprites.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-# Download Pokemon sprites from PokemonDB.net
+# Download Pokemon sprites.
 #
-# Download Pokemon sprites by name from PokemonDB.net into
-# local directory.
+# Download Pokemon sprites by first querying PokeAPI for Pokemon
+# names and types, then downloading sprites by name from PokemonDB.net
+# into a local directory.
 #
 
+BASE_API_URL='https://pokeapi.co/api/v2/pokemon'
 BASE_IMAGE_URL='https://img.pokemondb.net/sprites/sword-shield/icon'
 SPRITES_DIRECTORY='./assets/sprites'
 
 for ((pokemon_id=1; pokemon_id<=25; pokemon_id++)) ; do
-    api_response=$(curl -s "https://pokeapi.co/api/v2/pokemon/${pokemon_id}")
+    api_response=$(curl -s "${BASE_API_URL}/${pokemon_id}")
     pokemon_name=$(jq -r '.name' <<< ${api_response})
     pokemon_type=$(jq -r '.types[] | select (.slot==1) | .type.name' <<< ${api_response})
 
